7226a6e8ab142ab183dd7f4a1a4299aa8ed51e66
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2022-02-10 20:30:52 +0100

    Added resource files: Single molecule configs

--- cpfun.sh
+++ cpfun.sh
@@ -5,3 +5,4 @@ SEPLIB_PATH=/home/jschmidt/software/seplib/
 cp -r $SEPLIB_PATH/include/* src/include/
 cp -r $SEPLIB_PATH/source/* src/source/
 cp -r $SEPLIB_PATH/tools/* src/tools/
+cp -r $SEPLIB_PATH/tools/molconfigs/* resources/
