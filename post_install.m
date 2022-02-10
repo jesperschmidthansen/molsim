1bae80302747ce8c8e90ac6032d9c5e6a9b9e07b
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2022-02-10 20:41:27 +0100

    Added a bit more post installation info

---
+++ post_install.m
@@ -0,0 +1,9 @@
+
+function post_install(dummy)
+
+  printf("molsim installed - hopefully without errors\n");
+  printf("Try command \n >> molsim('hello')  \n in order to shake hands");
+  printf("Try command \n >> molsim_runlj \n to run example simulation");
+  printf("Package doc/ directory provides the molsim reference sheet. \n");
+  
+endfunction
