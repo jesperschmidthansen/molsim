fa40e04dee169c89781c587ce55d8d4480431fcc
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2022-05-01 07:04:04 +0200

    Added get<-mass TAKE 2

--- src/molsim.c
+++ src/molsim.c
@@ -553,16 +553,15 @@ void action_get(mxArray **plhs, int nrhs, const mxArray **prhs){
     
     free(types);
   }
-   // Types
+  // Mass
   else if ( strcmp("mass", specifier)==0 ){
     plhs[0] = mxCreateDoubleMatrix(natoms, 1, mxREAL);
 
-    double *mass = mexGetPr(plhs[0]);
+    double *mass = mxGetPr(plhs[0]);
     
     for ( int n=0; n<natoms; n++ ) mass[n] = atoms[n].m;
   }
- 
-  // Energies
+   // Energies
   else if ( strcmp("energies", specifier)==0 ){
     plhs[0] = mxCreateDoubleMatrix(1, 2, mxREAL);
     
