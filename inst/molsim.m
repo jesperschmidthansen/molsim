bbfc4713e8bbb0c9004bfba0f3fd1e6d73e0f6a2
Jesper Schmidt Hansen <jschmidt@ruc.dk>
2022-09-22 17:02:18 +0200

    Final(?) revision before release of 0.9.5

--- inst/molsim.m
+++ inst/molsim.m
@@ -1,5 +1,5 @@
 %
 % usage molsim(action, specifier, arg1, arg2, ...)
 %
-% See 'molsim.pdf' reference quick sheet (installed in package directory under doc/). 
+% See 'tutorial.pdf' reference quick sheet (installed in package directory under doc/). 
 %
