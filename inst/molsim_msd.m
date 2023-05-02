3231f3a29fae8d6d6f582c45a52d15fb3a394704
Jesper Hansen <jschmidt@nb-dk1081102.ruc.dk>
2023-05-02 16:36:22 +0200

    Changes to molsim_msd

--- inst/molsim_msd.m
+++ inst/molsim_msd.m
@@ -1,14 +1,14 @@
 
 
 
-function [msd scatt] = molsim_msd(ptype='A', numbwave=10, maxconfidx=10000)
+function [msd scatt _time] = molsim_msd(ptype='A', numbwave=10, haveCrossing=true, maxconfidx=10000)
 
 	
 	if ( !exist("molsim_readxyz") )
 		error("molsim_msd dependent on molsim_evcorr and molsim_readxyz");
 	endif
 	
-	indx = 0; 
+	indx = 0; _time(1)=0; 
 	while ( 1 )
 		
 		filename = sprintf("molsim-%05d.xyz", indx);
@@ -20,11 +20,15 @@ function [msd scatt] = molsim_msd(ptype='A', numbwave=10, maxconfidx=10000)
 		[npart, parttypes, pos, vel, mass, charge, lbox] = molsim_readxyz(filename, 'sep'); 
 		
 		if ( indx==0 )
-			pidx = find( parttypes==ptype ); nptypes = length(pidx);
+			pidx = find( parttypes==ptype ); 
+			nptypes = length(pidx);
 			prevPos = pos0 = pos(pidx,:);
-			crossings = zeros(nptypes, 3);
 			
-			waves = 2*pi.*[1:1:numbwave]./lbox(1);
+			if ( !haveCrossing )
+				crossings = zeros(nptypes, 3);
+			endif
+			
+			waves = 2*pi.*[1:1:numbwave]./lbox(2);
 			
 			printf("Found %d particles of type %c\n", nptypes, ptype);
 			fflush(stdout);
@@ -32,19 +36,25 @@ function [msd scatt] = molsim_msd(ptype='A', numbwave=10, maxconfidx=10000)
 		
 		dr = pos(pidx,:) - prevPos;
 		
-		for k=1:3
-			indxWrap = find( dr(:,k) > 0.5*lbox(k) );
-			crossings(indxWrap,k) += 1;
-			
-			indxWrap = find( dr(:,k) < -0.5*lbox(k) );
-			crossings(indxWrap,k) += -1;
-		endfor
-		prevPos = pos(pidx,:);
-		
-		dr = pos(pidx,:) - crossings.*lbox - pos0;
+		if ( !haveCrossing )  
+			for k=1:3
+				indxWrap = find( dr(:,k) > 0.5*lbox(k) );
+				crossings(indxWrap,k) += 1;
 				
+				indxWrap = find( dr(:,k) < -0.5*lbox(k) );
+				crossings(indxWrap,k) += -1;
+			endfor
+		else
+			datacross = load(sprintf("crossings-%05d.dat", indx));
+			_time(indx+1) = datacross(1,1);
+			crossings = datacross(pidx+1,:);
+		end
+
+		prevPos = pos(pidx,:);
+						
+		dr = pos(pidx,:) + crossings.*lbox - pos0;
 		sd = sum(dot(dr', dr'))./nptypes;
-		
+				
 		## MSD
 		indx++; msd(indx) = sd;
 		
@@ -63,90 +73,3 @@ function [msd scatt] = molsim_msd(ptype='A', numbwave=10, maxconfidx=10000)
 	printf("\n");
 endfunction 
 
-
-
-#{
-  int index = sptr->i;
-  if ( index == 0 ){
-    for ( int n=0; n<sys.npart; n++ ){
-      for ( int k=0; k<3; k++ ) {
-		sptr->prev_pos[n][k] = sptr->pos0[n][k] = atom[n].x[k];
-		sptr->crossings[n][k] = 0;
-      }
-    }
-  }
-
-  double sd = 0, qd=0.0, dr[3]={0.0};
-  for ( int n=0; n<sys.npart; n++ ){
-    if ( atom[n].type == sptr->type ){
-      for ( int k=0; k<3; k++ ){
-
-	dr[k] = atom[n].x[k] + sptr->crossings[n][k]*sys.length[k] 
-	  - sptr->pos0[n][k];
-      }
-
-      double a = sep_dot(dr, dr, 3);
-      
-      sd += a;
-      qd += a*a;
-    }
-  }
-  
-  for ( int i=0; i<sptr->nk; i++ ){
-    for ( int n=0; n<sys.npart; n++ ){
-      if ( atom[n].type == sptr->type ){
-	dr[0] = atom[n].x[0] + sptr->crossings[n][0]*sys.length[0] 
-	  - sptr->pos0[n][0];
-	sptr->Fs[index][i] += cexp(I*sptr->k[i]*dr[0]);     
-      }
-    }
-  }
-
-  sptr->msd[index]   += sd;
-  sptr->msdsq[index] += qd;
-  index ++;
-   
-  if ( index == sptr->lvec ){
-    sptr->nsample++;
-    const int ntype = sep_count_type(atom, sptr->type, sptr->npart);
-
-    FILE *fout = fopen("msd.dat", "w");
-    if ( fout==NULL )
-      sep_error("%s at line %d: Couldn't open file", __func__, __LINE__);
-	   
-    for ( int n=0; n<sptr->lvec; n++ ){      
-      fprintf(fout, "%f %f \n",
-	      sptr->time[n], sptr->msd[n]/(ntype*sptr->nsample));
-    }
-    fclose(fout);
-    
-    fout = fopen("msd-gaussparam.dat", "w");
-    if ( fout==NULL )
-      sep_error("%s at line %d: Couldn't open file", __func__, __LINE__);
-  
-    for ( int n=0; n<sptr->lvec; n++ ){
-      double a = sptr->msdsq[n]/(ntype*sptr->nsample);
-      double b = sep_Sq(sptr->msd[n]/(ntype*sptr->nsample));
-      
-      fprintf(fout, "%f %f \n", sptr->time[n], 3.0*a/(5.0*b)-1.);
-    }
-    fclose(fout);
-    
-
-    fout = fopen("msd-incoherent.dat", "w");
-    if ( fout==NULL )
-      sep_error("%s at line %d: Couldn't open file", __func__, __LINE__);
-
-    for ( int n=0; n<sptr->lvec; n++ ){      
-      fprintf(fout, "%f ", sptr->time[n]);
-      for ( int i=0; i<sptr->nk; i++ )
-	fprintf(fout, "%f ", creal(sptr->Fs[n][i])/(sptr->nsample*ntype));
-      fprintf(fout, "\n");
-    }
-    fclose(fout);
-    index = 0;
-  }
-
-  sptr->i = index;
-}
-#}
