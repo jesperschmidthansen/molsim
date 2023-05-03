b3e772bd8cf866dc867ed87f2a53c0bc143dc74e
Jesper Hansen <jschmidt@nb-dk1081102.ruc.dk>
2023-05-03 14:01:09 +0200

    Sec. update

--- inst/molsim_msd.m
+++ inst/molsim_msd.m
@@ -8,7 +8,13 @@ function [msd scatt _time] = molsim_msd(ptype='A', numbwave=10, haveCrossing=tru
 		error("molsim_msd dependent on molsim_evcorr and molsim_readxyz");
 	endif
 	
-	indx = 0; _time(1)=0; 
+	if ( nargin > 4 )
+		error("Usage: molsim_msd(part. type, no. wavevectors, have crossing files, max. configurations)");
+	endif
+	
+	_time=[];
+	
+	indx = 0;  
 	while ( 1 )
 		
 		filename = sprintf("molsim-%05d.xyz", indx);
@@ -32,42 +38,51 @@ function [msd scatt _time] = molsim_msd(ptype='A', numbwave=10, haveCrossing=tru
 			
 			printf("Found %d particles of type %c\n", nptypes, ptype);
 			fflush(stdout);
+		else 
+			if ( !haveCrossing )  
+				## Attempts to find the true distance from prev. positions
+				dr = pos(pidx,:) - prevPos;
+				for k=1:3
+					indxWrap = find( dr(:,k) > 0.5*lbox(k) );
+					crossings(indxWrap,k) += 1;
+					
+					indxWrap = find( dr(:,k) < -0.5*lbox(k) );
+					crossings(indxWrap,k) += -1;
+				endfor
+				
+				prevPos = pos(pidx,:);
+				
+			else
+				filenameCross = sprintf("crossings-%05d.dat", indx);
+				if  ( !exist(filenameCross) )
+					error("Cannot find crossing files\n");
+				endif
+				
+				datacross = load(filenameCross);
+				_time(indx) = datacross(1,1);
+				crossings = datacross(pidx+1,:);
+			endif
+	
+			dr = pos(pidx,:) + crossings.*lbox - pos0;
+			sd = sum(dot(dr', dr'))./nptypes;
+					
+			## MSD
+			msd(indx) = sd;
+			
+			## SCATT
+			for nk=1:numbwave
+				sumsc = 0.0;
+				for np=1:nptypes
+					sumsc = sumsc + real(exp(-I.*waves(nk)*dr(np,1)));
+				end
+				scatt(indx, nk) = sumsc/nptypes;
+			end
+			
 		endif
 		
-		dr = pos(pidx,:) - prevPos;
+		indx++;
 		
-		if ( !haveCrossing )  
-			for k=1:3
-				indxWrap = find( dr(:,k) > 0.5*lbox(k) );
-				crossings(indxWrap,k) += 1;
-				
-				indxWrap = find( dr(:,k) < -0.5*lbox(k) );
-				crossings(indxWrap,k) += -1;
-			endfor
-		else
-			datacross = load(sprintf("crossings-%05d.dat", indx));
-			_time(indx+1) = datacross(1,1);
-			crossings = datacross(pidx+1,:);
-		end
-
-		prevPos = pos(pidx,:);
-						
-		dr = pos(pidx,:) + crossings.*lbox - pos0;
-		sd = sum(dot(dr', dr'))./nptypes;
-				
-		## MSD
-		indx++; msd(indx) = sd;
-		
-		## SCATT
-		for nk=1:numbwave
-			sumsc = 0.0;
-			for np=1:nptypes
-				sumsc = sumsc + real(exp(-I.*waves(nk)*dr(np,1)));
-			end
-			scatt(indx, nk) = sumsc/nptypes;
-		end
-		
-		printf("\rProcessed %d  ", indx);fflush(stdout);
+		printf("\rProcessed %d  ", indx); fflush(stdout);
 	endwhile 
 	
 	printf("\n");
