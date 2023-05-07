fb93aafbd57a04a8443f7479e7a65f9991a9ce86
Jesper Hansen <jschmidt@nb-dk1081102.ruc.dk>
2023-05-07 17:59:11 +0200

    Issues with github that does not include molsim_runlj in zip - removed some post install advice

--- post_install.m
+++ post_install.m
@@ -3,7 +3,6 @@ function post_install(dummy)
   
   printf("\nmolsim installed - hopefully without errors\n");
   printf("Try command \n >> molsim('hello')\nin order to shake hands\n");
-  printf("Try command \n >> molsim_runlj \nto run example simulation\n");
   printf("Reference sheet can be found under package doc/ directory. \n");
   
 endfunction
