fdb4ab04d0c2f1baf30c8167b315f2b5b585161a
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2025-04-06 19:04:11 +0200

    Dev. update

--- newmolsim/pre_install.m
+++ newmolsim/pre_install.m
@@ -6,6 +6,6 @@ function pre_install(dummy)
 
 	run("src/mhtxt.m");
 	system("mv src/ms_*.m inst/");
-
+	system("mv src/mhtxt.m inst/");	
 end
 
--- newmolsim/src/mhtxt.m
+++ newmolsim/src/mhtxt.m
@@ -1,14 +1,18 @@
 #
-# Script to generate help txt - rather explaine why there is no help txt
+# Usage: mhtxt()
 #
-files = glob("*.c");
+# Help text generator
+#
+function mhtxt()
+	files = glob("*.c");
 
-for n=1:length(files)
-	fname = files{n};
-	nfname = sprintf("%s.m", fname(1:end-2));	
+	for n=1:length(files)
+		fname = files{n};
+		nfname = sprintf("%s.m", fname(1:end-2));	
 
-	fid = fopen(nfname, "w");
-	fprintf(fid, "# This function is a part of the molsim package\n");
-	fprintf(fid, "# It is likely a mex function for internal package use and there is currently no help text\n");
-	fclose(fid);
+		fid = fopen(nfname, "w");
+		fprintf(fid, "#%s This function is a part of the molsim package\n", fname(1:end-2));
+		fprintf(fid, "# It is likely a mex function for internal package use and there is currently no help text\n");
+		fclose(fid);
+	end
 end
