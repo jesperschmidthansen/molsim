4efb48bfa126b4ca6dfcf7c791a059225e0f0877
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2025-05-09 13:13:44 +0200

    Dev. update

--- newmolsim/src/mhtxt.m
+++ newmolsim/src/mhtxt.m
@@ -1,18 +1,14 @@
 #
-# Usage: mhtxt()
-#
 # Help text generator
 #
-function mhtxt()
-	files = glob("*.c");
+files = glob("*.c");
 
-	for n=1:length(files)
-		fname = files{n};
-		nfname = sprintf("%s.m", fname(1:end-2));	
+for n=1:length(files)
+	fname = files{n};
+	nfname = sprintf("%s.m", fname(1:end-2));	
 
-		fid = fopen(nfname, "w");
-		fprintf(fid, "#%s This function is a part of the molsim package\n", fname(1:end-2));
-		fprintf(fid, "# It is likely a mex function for internal package use and there is currently no help text\n");
-		fclose(fid);
-	end
+	fid = fopen(nfname, "w");
+	fprintf(fid, "#%s This function is a part of the molsim package\n", fname(1:end-2));
+	fprintf(fid, "# It is likely a mex function for internal package use and there is currently no help text\n");
+	fclose(fid);
 end
