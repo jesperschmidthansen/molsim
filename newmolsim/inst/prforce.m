ed0d7de5b32f05206c7ba6947eeb3ada959e0cd2
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2025-10-26 19:33:24 +0100

    Removed oct file calcdr2

--- newmolsim/inst/prforce.m
+++ newmolsim/inst/prforce.m
@@ -39,15 +39,19 @@ classdef prforce < handle
 			end
 	
 			if this.first_call # first_call set to true in integrator
-				atoms.f = zeros(atoms.natoms, 3); 
-				dr2 = ms_calcdr2(atoms.r, atoms.r0, atoms.lbox);
+				
+				atoms.f = zeros(atoms.natoms, 3);
+				dr2 = ms_calcdr2(atoms.r, atoms.r0, atoms.lbox, atoms.natoms);
+				
 				if this.first_call_simulation || dr2 > this.skin*this.skin 
 					ms_neighblist(atoms.nblist, atoms.r, atoms.r0, atoms.lbox, this.max_cutoff, 
 									this.skin, atoms.natoms, atoms.exclude);
 					this.neighb_updates ++;
 				end
+				
 				this.first_call = false;
 				this.first_call_simulation = false;
+				
 			end
 		end
 
