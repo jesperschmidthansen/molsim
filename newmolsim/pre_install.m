d53e73efbe3dfefedb6f6c464f9dd00d8e20ca9d
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2026-02-25 20:37:11 +0100

    Added warning in pre_install.m

---
+++ newmolsim/pre_install.m
@@ -0,0 +1,11 @@
+
+function pre_install(dummy)
+
+	printf("IMPORTANT: Warnings are issued because mex files are not documented.\n");
+	printf("This problem is not yet resolved, but will have no effect on the package functionality.\n");
+	printf("See project's GitHub-page for documentation. Continuing... \n");	
+	
+	fflush(stdout); 
+	pause(5);
+
+end
