c48bac186fa10858ff8ba7e61483a87146868e30
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2026-03-14 08:47:11 +0100

    Added comments in lj.m and butane.m

--- examples/butane.m
+++ examples/butane.m
@@ -1,58 +1,72 @@
 
-
 clear all;
 
+# Remove if script is run after package installation 
 addpath("../inst/"); addpath("../src/");  addpath("../resources/setup/");
 
-
-niter = 1e4; dt = 1e-3;
+# Variables holding number of MD loops, time step size, simulation temperature and density 
+nloops = 1e4; dt = 1e-3;
 temp0 = 3.0; dens0 = 1.49;
 
-system("rm *top");
+# Write configuation and topology files: start.xyz bonds.top, angles.top, dihedrals.top  
+# Configuration is 10x10x10 molecules with center-of-mass distance of 4
+system("rm -f *top");
 molconf("../resources/molconf/butane.xyz", "../resources/molconf/butane.top", [10 10 10], 4.0);
 
+# Instance of molsim 
 sim = molsim();
+
+# Set configureation from start.xyz
 sim.setconf("start.xyz");
 
+# Set integrator time step and thermostat temperature
 sim.integrator.dt = dt;
 sim.thermostat.temperature= temp0;
 
+# Reads in topology files 
 sim.settop();
 
+# Sets the bond spring constant and zero force bond length - only one bond type, namely, 0
 nbonds = sim.bonds.nbonds;
 sim.bonds.springs = 33e3*ones(nbonds,1); sim.bonds.l0 = 0.4*ones(nbonds,1); 
 
+# Sets the angle spring constant and zero force angle (radians) - only one angle type, namely, 0
 nangles = sim.angles.nangles;	
 sim.angles.springs = 866*ones(nangles,1); sim.angles.a0 = 1.9*ones(nangles, 1);
 
+# Sets the dihedral coefficients for the Ryckert-Belleman mode - only one dihedral, namely, 0 
 ndihedrals = sim.dihedrals.ndihedrals;
 for n=1:ndihedrals
 	sim.dihedrals.coeffs(n,:) = [15.5000,  20.3050, -21.9170, -5.1150,  43.8340, -52.6070]; 
 end
 
+# Exclude pair forces for atoms in same dihedral angle (here this is the same as the molecule) 
 sim.atoms.setexclusions(sim.dihedrals.pidx, "dihedrals");
 
-ekin = zeros(niter,1); epot = zeros(niter,1);
-
-for n=1:niter
+# Main MD loop
+ekin = zeros(nloops,1); epot = zeros(nloops,1);
+for n=1:nloops
+	# Calculate the pair forces 	
 	epot(n) = sim.lennardjones("CC", [2.5, 1.0, 1.0, 1.0]);   
 	
+	# Calcuate the intra-molecular forces
 	epot(n) += sim.harmonicbond(0);
 	epot(n) += sim.harmonicangle(0);
 	epot(n) += sim.ryckbell(0);
 
+	# Apply Nose-Hoover thermostat
 	sim.nosehoover();
+
+	# Integrate forward in time
 	ekin(n) = sim.leapfrog();
 	
+	# Rescale simulation box to obtain desired density
 	if rem(n, 10)==0
 		sim.scalebox(dens0);
 	end
 
 end
-sim.natoms/sim.volume
 
+# Save the final configuration
 sim.atoms.save("final.xyz");
-
-plot([1:niter], ekin/sim.natoms, [1:niter], epot/sim.natoms, [1:niter], (epot+ekin)./sim.natoms);
-pause(10);
 
--- examples/lj.m
+++ examples/lj.m
@@ -1,18 +1,30 @@
 clear all;
 
+# Remove if script is run after package installation 
 addpath("../inst/"); addpath("../src/");
 
-niter = 1e4;
+# Number of MD loops
+nloops = 1e4;
 
+# Instance of molsim object
 sim = molsim();
+
+# Set number of particles and simulation box lengths in each direction. 
+# Temperature is initially set to 1.0
 sim.setconf([10,10,10], [10.557, 10.557, 10.557], 1.0);
 
-ekin = zeros(niter,1); epot = zeros(niter,1);
-for n=1:niter
+# Main MD loop
+ekin = zeros(nloops,1); epot = zeros(nloops,1);
+for n=1:nloops
+	# Calculates the forces acting on particles of type A - default type
+	# Lennard-Jones parameters are [cutoff, epsilon, sigma, aW]
 	epot(n) = sim.lennardjones("AA", [2.5, 1.0, 1.0, 1.0]);   
+
+	# Integrate forward in time using the leap-frog algorithm
 	ekin(n) = sim.leapfrog();
 end
 
-plot([1:niter], ekin/sim.natoms, [1:niter], epot/sim.natoms, [1:niter], (epot+ekin)./sim.natoms);
-pause(10);
+# Plot the energies
+plot([1:nloops], ekin/sim.natoms, [1:nloops], epot/sim.natoms, [1:nloops], (epot+ekin)./sim.natoms);
+
 
